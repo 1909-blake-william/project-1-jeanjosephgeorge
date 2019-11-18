b2691b3cf3904002db7397353ef9b6dc6f9eb5ec
jeanjosephgeorge <jean.joseph@outlook.com>
2019-11-17 19:07:41 -0500

    Sunday evening commit

--- API/src/main/webapp/filter.js
+++ API/src/main/webapp/filter.js
@@ -1,33 +1,31 @@
 function filterSelection(val){
-
+    //Turns Everything on if selection is ALL
     if(val == 'All'){
-
-    let alls = document.getElementsByTagName('tr')
-    for(let a = 0; a<alls.length; a++){
-            alls[a].style.display = 'table-row';
+        let alls = document.getElementsByTagName('tr')
+        for(let a = 0; a<alls.length; a++){
+        alls[a].style.display = 'table-row';
         }
     } else {
-    let all = document.getElementsByTagName('tr')
-    for(let a = 0; a<all.length; a++){
-        if(all[a].id == 'mainRow'){
-        } else {
-            all[a].style.display = 'none';
-        }
+
+        //Onclick, first turns all off
+        let all = document.getElementsByTagName('tr')
+        for(let a = 0; a<all.length; a++){
+            if(all[a].id == 'mainRow'){
+            } else {
+                all[a].style.display = 'none';
+            }
     }
     let rows = document.getElementsByClassName(val);
     console.log(rows);
 
-    for(let i = 0; i<rows.length; i++){
-        // console.log(rows[i].style.display);
-        if (rows[i].style.display == 'table-row'){
-                    rows[i].style.display = 'none';
+        //Then turns your selection On
+        for(let i = 0; i<rows.length; i++){
+            if (rows[i].style.display == 'table-row'){
+                rows[i].style.display = 'none';
                 } else {
                     rows[i].style.display = 'table-row';
                 }
         }
     }
-    
-    
-    
 }
 
